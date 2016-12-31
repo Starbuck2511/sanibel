701d5a1d770afbb12d6b341eb8638022ea10f4e8
starbuck <kai.boeker@web.de>
2016-12-31 11:39:28 +0100

    Added app config dist file

---
+++ teammates/src/app/app.config.dist.ts
@@ -0,0 +1,9 @@
+export class AppConfig {
+    public  static FIREBASE_CONFIG: any = {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        storageBucket: "",
+        messagingSenderId: ""
+    };
+}
