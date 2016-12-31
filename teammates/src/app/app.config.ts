28d30afedd10712a17a04c552acc5469a30baa04
starbuck <kai.boeker@web.de>
2016-12-31 11:27:15 +0100

    Removed app config from version control

--- teammates/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-export class AppConfig {
-    public  static FIREBASE_CONFIG: any = {
-        apiKey: "AIzaSyBOGOQUrI3iLu8yKzcOCqQGPhk0St5GnY0",
-        authDomain: "teammates-73d62.firebaseapp.com",
-        databaseURL: "https://teammates-73d62.firebaseio.com",
-        storageBucket: "teammates-73d62.appspot.com",
-        //messagingSenderId: "739745043793"
-    };
-}
